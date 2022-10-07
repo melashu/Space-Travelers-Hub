da51e64fc6b59eee4a58b7866b5b3572e5f6cb07
melashu <meshu102@outlook.com>
2022-10-07 15:07:09 +0300

    create store

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,12 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+// import dragonreducer from "./dragonreducer";
+import missionReducer from "./reducers";
 
 const store = configureStore({
-    reducer: {
-        mission: {},
-        rokets: {}
-    }
+  reducer: {
+    mission: missionReducer,
+    // dragon:dragonreducer,
+    rokets: {},
+  },
 });
 
 export default store;
-
