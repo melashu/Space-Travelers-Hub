e1d87515e04fd2e93c835e2d6b0ca5c0116f0098
melashu <meshu102@outlook.com>
2022-10-07 15:07:45 +0300

    create routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import { Routes, Route } from 'react-router-dom';
+import Header from './Compontes/Header/Header';
+import Mission from './Compontes/Mission';
+import Profile from './Compontes/Profile';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <Header />
+      <Routes>
+        <Route to="/">
+          <Route index element={null} />
+          <Route path="mission">
+            <Route index element={<Mission />} />
+          </Route>
+          <Route path="profile">
+            <Route index element={<Profile />} />
+          </Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
