1b717b11b5db116d49d7b148ea701e2c5e9d5d1b
melashu <meshu102@outlook.com>
2022-10-08 23:23:50 +0300

    add dragon

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import dragonreducer from "./dragonreducer";
 
 import missionReducer from "./reducers";
 import rocketReducer from "./rocketReducer";
@@ -6,8 +7,11 @@ import rocketReducer from "./rocketReducer";
 const store = configureStore({
   reducer: {
     mission: missionReducer,
+    dragon:dragonreducer,
     rockets: rocketReducer,
   },
 });
 
 export default store;
+
+//machsed
---
+++ src/Store/dragonreducer.jsx
@@ -0,0 +1,67 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+//Async
+const dragonThunk = createAsyncThunk("dragon", async () => {
+  try {
+    try {
+      const respons = await axios.get("https://api.spacexdata.com/v3/dragons");
+      return respons.data;
+    } catch (error) {
+      return error;
+    }
+  } catch (error) {}
+});
+
+
+const initialState = {
+  dragons: [],
+  dragonloading: "idel",
+};
+const dragonReducer = createSlice({
+  name: "dragon",
+  initialState,
+  reducers: {
+    reserveDragon: (state, action) => {
+      const newState = state.dragons.map((dragon) => {
+        if (dragon.id === action.payload) {
+          const newState = { ...dragon, reserved: true };
+          return newState;
+        }
+        return dragon;
+      });
+      state.dragons = newState;
+    },
+    cancleDragon: (state, action) => {
+      const newState = state.dragons.map((dragon) => {
+        if (dragon.id !== action.payload) {
+          return dragon;
+        }
+        const newState = { ...dragon, reserved: false };
+        return newState;
+      });
+      state.dragons = newState;
+    },
+  },
+  extraReducers: {
+    //dragon/fulfilled
+    [dragonThunk.fulfilled]: (state, { payload }) => {
+      state.dragons = payload;
+    },
+    [dragonThunk.pending]: (state) => {
+      state.dragonloading = "pending";
+    },
+    [dragonThunk.rejected]: (state) => {
+      state.dragonloading = "rejected";
+    },
+  },
+});
+
+const getDragonState = (state) => state.dragon.dragons;
+const getDragonLoading = (state) => state.dragon.dragonloading;
+export const { reserveDragon, cancleDragon } = dragonReducer.actions;
+export {
+  dragonThunk,
+  getDragonState,
+  getDragonLoading,
+};
+export default dragonReducer.reducer;
