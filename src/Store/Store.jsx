ef060d86127fe31c6e78b7f36d2d42e52c04dc5a
melashu <meshu102@outlook.com>
2022-10-09 12:42:20 +0300

    Add persist storage

--- src/Store/Store.jsx
+++ src/Store/Store.jsx
@@ -1,17 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
-import dragonreducer from "./dragonreducer";
+/* eslint-disable import/no-extraneous-dependencies */
 
-import missionReducer from "./reducers";
-import rocketReducer from "./rocketReducer";
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistStore, persistReducer } from 'redux-persist';
+import dragonreducer from './dragonreducer';
+import missionReducer from './reducers';
+import rocketReducer from './rocketReducer';
 
+const rootReducer = combineReducers({
+  mission: missionReducer,
+  dragon: dragonreducer,
+  rockets: rocketReducer,
+});
+const config = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(config, rootReducer);
 const store = configureStore({
-  reducer: {
-    mission: missionReducer,
-    dragon:dragonreducer,
-    rockets: rocketReducer,
-  },
+  reducer: persistedReducer,
 });
 
+const persistedStore = persistStore(store);
+export { persistedStore };
 export default store;
-
-//machsed
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import App from './App';
-import store from './Store/Store';
+import store, { persistedStore } from './Store/Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading="null" persistor={persistedStore}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
